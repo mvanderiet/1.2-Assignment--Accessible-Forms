59d0793f27ee3b68a91d5c7c4163894bc56de287
mvanderiet <mvanderi@asu.edu>
2025-07-18 13:43:06 -0600

    Update submittedValues.php

--- submittedValues.php
+++ submittedValues.php
@@ -1,8 +1,7 @@
 <html>
 <body>
 
-Thank you <?php echo $_POST["fullName"]; ?><br>
-Your email address is: <?php echo $_POST["emailAddress"]; ?>
+Thank you for your submission, <?php echo $_POST["fullName"]; ?><br>
 
 </body>
 </html>
